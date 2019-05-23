d6bb0f115ed0d59fa73c633481a0fe3cc1ead388
Carolina Estrela <estrela.carolina@bol.com.br>
2019-05-22 22:21:57 -0300

    Fazendo o POST  do formulário

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-const box= document.querySelector('.maravilhosas_box');
+const box= document.querySelector('.maravilhosas__box');
+const button=document.getElementById("submit");
 
-fetch('https://theblackwomanhistory.firebaseio.com/.json')
+button.addEventListener("click", (evento) =>{
+evento.preventDefault();
 
+const nome=document.getElementById("title").value
+const img=document.getElementById("imagem").value
+ 
+fetch('http://localhost:5001/maravilhosas/',{
+    method:'POST',
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+    'title': nome,
+    
+    })
+})
 .then((response) => {
     return response.json();
 })
 .then((data)=>{
-    console.log(content);
-
-    data.content.forEach(mulher =>{
+    data.content.forEach((mulher) =>{
 
         let perfil  = document.createElement('div');
-        box.setAttribute('class','maravilhosas__perfil');
+        perfil.setAttribute('class','maravilhosas__perfil');
         box.appendChild(perfil);
 
         let link = document.createElement('a');
-        link.href= '#!';
+        link.setAttribute ('href', '#');
         perfil.appendChild(link);
 
         let imagem =document.createElement('img');
         imagem.setAttribute('class', 'img-responsive');
-        imagem.src='#!';
+        if (mulher.metadata && mulher.metadata.image){
+            imagem.setAttribute('src', mulher.metadata.image.url); 
+        }else{ 
+            imagem.setAttribute('src', 'img/img-mulher.png');
+        }
         link.appendChild(imagem);
 
         let nome=document.createElement('p');
@@ -29,6 +47,7 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
 
     })
 })
-.catch((erro){
-    console.log(erro)
+.catch((erro)=>{
+    console.log(erro);
+    })
 })
